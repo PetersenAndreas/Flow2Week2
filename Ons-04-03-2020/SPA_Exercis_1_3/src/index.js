06f62b849161ed903ee9e2fcebdcb446f8c30226
Andreas <andreas.petersen.3540@gmail.com>
2020-03-04 17:37:59 +0100

    Done assignment 1-3

---
+++ Ons-04-03-2020/SPA_Exercis_1_3/src/index.js
@@ -0,0 +1,70 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import jokes from "./jokes";
+
+const allJokes = jokes.getJokes().map(joke => "<li>" + joke + "</li>");
+document.getElementById("jokes").innerHTML = allJokes.join("");
+
+//Finding individual jokes
+const getJokeByIDElement = document.getElementById("joke_id");
+const jokeByIdBtn = document.getElementById("joke_by_id_btn");
+jokeByIdBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const newJokeByIdText = jokes.getJokeById(getJokeByIDElement.value - 1);
+    document.getElementById("joke_id_text").innerHTML = newJokeByIdText;
+
+});
+
+//Adding new Jokes
+const newJokeInsert = document.getElementById("newJoke");
+const newJokeBtn = document.getElementById("newJokeBtn");
+newJokeBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    jokes.addJoke(newJokeInsert.value);
+    const allJokes = jokes.getJokes().map(joke => "<li>" + joke + "</li>");
+    document.getElementById("jokes").innerHTML = allJokes.join("");
+
+});
+
+//2. Small application to display a quote of the hour
+const getQoutOfTheHourBtn = document.getElementById("quoteBtn");
+getQoutOfTheHourBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    fetchFunction();
+    //console.log("result" + result);
+
+});
+
+function fetchFunction() {
+    fetch('https://studypoints.info/jokes/api/jokes/period/hour')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data.joke);
+            document.getElementById("myQDiv").innerHTML = data.joke;
+        });
+};
+
+document.getElementById("clover").addEventListener("click", (event) => {
+    document.getElementById("cloverText").innerHTML = "Nej"
+});
+
+document.getElementById("north").addEventListener("click", (event) => {
+    event.stopPropagation();
+    document.getElementById("cloverText").innerHTML = "North"
+});
+
+document.getElementById("east").addEventListener("click", (event) => {
+    event.stopPropagation();
+    document.getElementById("cloverText").innerHTML = "East"
+});
+
+document.getElementById("south").addEventListener("click", (event) => {
+    event.stopPropagation();
+    document.getElementById("cloverText").innerHTML = "South"
+});
+
+document.getElementById("west").addEventListener("click", (event) => {
+    event.stopPropagation();
+    document.getElementById("cloverText").innerHTML = "West"
+});
